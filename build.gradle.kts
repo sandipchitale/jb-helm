302e103cf1b001023cffba88da0c749f47343f98
Sandip Chitale <sandipchitale@gmail.com>
2025-11-05 20:10:06 -0800

    Update for 2025.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij.platform") version "2.5.0"
+    id("org.jetbrains.kotlin.jvm") version "2.1.20"
+    id("org.jetbrains.intellij.platform") version "2.10.4"
 }
 
 group = "dev.sandipchitale"
-version = "1.0.13"
+version = "1.0.14"
 
 repositories {
     mavenCentral()
@@ -19,39 +19,39 @@ dependencies {
     implementation("io.kubernetes:client-java:22.0.0")
 
     intellijPlatform {
-        if (project.hasProperty("runIde_ideDir")) {
-            local("${project.extra["runIde_ideDir"]}")
-        } else {
-            intellijIdeaCommunity("2024.2")
+        intellijIdea("253-EAP-SNAPSHOT") {
+            useInstaller = false
+        }
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+        bundledPlugin("org.jetbrains.plugins.terminal")
+    }
+}
+
+intellijPlatform {
+    buildSearchableOptions = false
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "253"
         }
 
-        // Declare dependency on the bundled Terminal plugin
-        bundledPlugin("org.jetbrains.plugins.terminal")
+        changeNotes = """
+            Initial version
+        """.trimIndent()
     }
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("242")
-        untilBuild.set("252.*")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
     }
 }
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
+
